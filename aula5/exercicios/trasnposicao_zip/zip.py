1d9e719868b2e8aea7fc043f0570aa9d7f45f621
Abioluz Robson Behrend <abioluz@gmail.com>
2020-07-04 11:43:42 -0300

    aula 05

--- aula5/exercicios/trasnposicao_zip/zip.py
+++ aula5/exercicios/trasnposicao_zip/zip.py
@@ -1,5 +1,6 @@
 # Abra a lista cadastro3.txt
-# Crie uma List Comprehension e zip que trate e converta cada pessoa em um
+# Crie uma List Comprehension e zip que trate e converta 
+# cada pessoa em um
 #  dicionário
 # Salve os dicionários em uma lista
 # Encontre todos que possuam idade de 18 a 25 anos.
